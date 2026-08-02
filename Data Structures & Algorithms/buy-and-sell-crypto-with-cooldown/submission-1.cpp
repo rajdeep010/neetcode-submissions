4072556973dac14d194463194576282a6d08aebe
Rajdeep Mallick <86741016+rajdeep010@users.noreply.github.com>
2026-08-03 00:20:12 +0530

    Bulk sync: 17 submissions

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int f(vector<int>& prices, int i, int buy, vector<vector<int>>& dp){
+        int n = prices.size();
+        if(i >= n)  return 0;
+        if(dp[i][buy] != -1)    return dp[i][buy];
+
+        int ans = 0;
+        // i can buy
+        if(buy){
+            int skip = f(prices,i+1,1,dp);
+            int doit = -prices[i]+f(prices,i+1,0,dp);
+            ans = max(skip,doit);
+        } else {
+            int skip = f(prices,i+1,0,dp);
+            int doit = prices[i]+f(prices,i+2,1,dp);
+            ans = max(skip,doit);
+        }
+
+        return dp[i][buy] = ans;
+    }
+
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2,-1));
+        // // dp[i][0] = max if sell
+
+        // for(int i = n-1; i >=0; i--){
+        //     dp[i][0] = max(dp[i+1][0], prices[i]+dp[i+2][1]);
+        //     dp[i][1] = max(dp[i+1][1], -prices[i]+dp[i+1][0]);
+        // }
+
+        // return dp[0][1];
+
+        return f(prices, 0,1,dp);
+    }
+};
